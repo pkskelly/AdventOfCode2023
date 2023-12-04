2595b69316b62af3317f5b46693c1f6f35021226
Pete Skelly <peteskelly@gmail.com>
2023-12-04 14:29:34 -0500

    Start of project

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace AdventOfCode2023 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ tests/DayOneTests.cs
@@ -0,0 +1,14 @@
+
+namespace AdventOfCode2023.Tests;
+
+[TestClass]
+public class DayOneTests
+{
+  
+    [TestMethod]
+    public void Should_SingleDigitInString()
+    {
+        Assert.IsTrue(true);
+    }
+
+}
