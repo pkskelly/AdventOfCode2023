66321d49d2a9505091dd80b90b5d96b05d21fdef
Pete Skelly <peteskelly@gmail.com>
2023-12-07 21:58:01 -0500

    Day 1 Done

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AdventOfCode2023
-2023 Advent of Code Repository
+
+[Advent of Code 2023](https://adventofcode.com/) in .NET 8
+
+## Day 1
+
+Humbling, tripped up by not recognizing the double number requirement for a single digit found. 
---
+++ src/Console/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.ComponentModel;
+using AdventOfCode2023.Library;
+
+namespace AdventOfCode2023.Console 
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            var calibrationSum = 0;
+            var filePath = args[0];
+            var fileExits = System.IO.File.Exists(filePath);
+            if ( fileExits == false)
+            {
+                System.Console.WriteLine("Please provide a valid input file.");
+                return;
+            }
+            string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            foreach (string line in lines)
+            {
+                var calibrationValue = Library.DayOneProcessor.GetCalibrationValue(line);
+                calibrationSum += calibrationValue;
+            }
+           
+           System.Console.WriteLine($"Calibration Sum = {calibrationSum}");
+        }
+    }
+}
