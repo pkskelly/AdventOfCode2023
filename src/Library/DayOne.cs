d384a41e02293b1698f44d313aada3726eec51d4
Pete Skelly <peteskelly@gmail.com>
2023-12-07 22:03:46 -0500

    Day 1 Done (#1)

---
+++ src/Library/DayOne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace AdventOfCode2023.Library
+{
+    public class DayOneProcessor
+    {
+        
+        // create a constant value that is a char array of the integers from 1 to 9
+        private static readonly char[] IntegerChars = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+          public static int GetCalibrationValue(string input)
+        {
+            var firstIntegerIndex = input.IndexOfAny(IntegerChars);
+            var lastIntegerIndex = input.LastIndexOfAny(IntegerChars);
+
+            // no numbers found
+            if (firstIntegerIndex == -1 || lastIntegerIndex == -1)
+            {
+                return -1;
+            }
+            var firstIntegerAsInt = int.Parse(input[firstIntegerIndex].ToString());
+            var lastIntegerAsInt = int.Parse(input[lastIntegerIndex].ToString());
+
+            return int.Parse($"{firstIntegerAsInt}{lastIntegerAsInt}");
+        }
+    }
+}
+
+
--- tests/DayOneTests.cs
+++ tests/DayOneTests.cs
@@ -1,14 +1,38 @@
+using AdventOfCode2023.Library;
 
-namespace AdventOfCode2023.Tests;
-
-[TestClass]
-public class DayOneTests
+namespace AdventOfCode2023.Tests
 {
-  
-    [TestMethod]
-    public void Should_SingleDigitInString()
-    {
-        Assert.IsTrue(true);
+
+    [TestClass]
+    public class DayOneTests
+    {      
+        [TestMethod]
+        public void NoDigit_Should_ReturnNegativeOne_Pass()
+        {
+            var day1Test = "DayOne";
+            Assert.IsTrue(DayOneProcessor.GetCalibrationValue(day1Test) == -1);
+        }
+ 
+        [TestMethod]
+        [DataRow("Day1",11 )]
+        [DataRow("Day1One",11)]
+        [DataRow("Day3One",33)]
+        [DataRow("9One",99)]
+        [DataRow("9On8e5",95)]
+        [DataRow("9On8e",98)]
+        [DataRow("treb7uchet",77)]
+        [DataRow("ldzpzgch325hsttqvmp9foureightmvsknbd",39)]
+        public void SingleInitialDigit_Should_Pass(string value, int expected)
+        {            
+            Assert.AreEqual(DayOneProcessor.GetCalibrationValue(value), expected);
+        }
+
+        [TestMethod]
+        [DataRow("S1ngleW8ffle",18)]
+        public void TwoDigits_Should_ReturnNumber_Pass(string value, int expected)
+        {            
+            Assert.IsTrue(DayOneProcessor.GetCalibrationValue(value) == expected);
+        }
     }
 
 }
